9caa631809bf8d1c015655d70afeac6d6e43a285
JITENDRA SINGH <jitendrasinghgurjar143@gmail.com>
2025-10-24 12:06:29 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountId = 12345;
+let accountEmail = "jitendrasinghgurjkaer143@gmail.com";
+var accountpassword = "1122334455";
+accountcity = "bareilly";
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountpassword, accountcity]);
